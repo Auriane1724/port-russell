b5c6104bf7c01af4b55e58bebdcf565ea23a826d
Auriane Oliveira <oliveira.auriane07@gmail.com>
2026-01-09 10:42:05 +0100

    Swagger: document Users CRUD

--- routes/api.users.routes.js
+++ routes/api.users.routes.js
@@ -9,6 +9,97 @@ const asyncHandler = require("../middlewares/asyncHandler");
 const u = require("../controllers/users.controller");
 
 const router = express.Router();
+/**
+ * @openapi
+ * /api/users:
+ *   get:
+ *     summary: Liste des utilisateurs
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: OK
+ *   post:
+ *     summary: Créer un utilisateur
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [email, password]
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Créé
+ *
+ * /api/users/{id}:
+ *   get:
+ *     summary: Détail d'un utilisateur
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: Introuvable
+ *   put:
+ *     summary: Mettre à jour un utilisateur
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *       404:
+ *         description: Introuvable
+ *   delete:
+ *     summary: Supprimer un utilisateur
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Supprimé
+ *       404:
+ *         description: Introuvable
+ */
 
 router.get("/users", requireAuth, asyncHandler(u.listUsers));
 router.get("/users/:id", requireAuth, asyncHandler(u.getUser));
